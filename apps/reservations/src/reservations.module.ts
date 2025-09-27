6c3ef6641c642014d703ff6af813cd99fe9f19bc
Daffa Alif Murtaja <daffa4lif@gmail.com>
2025-09-27 20:08:38 +0700

    refactor: streamline imports and remove LoggerModule configuration from ReservationsModule

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,23 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
-import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    DatabaseModule.forFeature([{ name: ReservationDocument.name, schema: ReservationSchema }]),
-    LoggerModule.forRoot({
-      pinoHttp: {
-        transport: {
-          target: 'pino-pretty',
-        },
-      },
-    }),
-  ],
+  imports: [DatabaseModule, DatabaseModule.forFeature([{ name: ReservationDocument.name, schema: ReservationSchema }]), LoggerModule],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
 })
